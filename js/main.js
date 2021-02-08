a9fbcd031f58b7d6e120a99213becedef17d2c6a
neneta0921 <65237907+neneta0921@users.noreply.github.com>
2021-02-08 20:18:20 +0900

    added scrollObserver

--- js/main.js
+++ js/main.js
@@ -3,6 +3,8 @@ let ready = false;
 const handlePhotos = new HandlePhotos();
 const getPhotos = () => handlePhotos.getPhotos();
 
+const scrollObserver = new ScrollObserver('#js-image-container', getPhotos, { once: false });
+
 // Check to see if scrolling near bottom of page, Load More Photos
 window.addEventListener('scroll', () => {
   if (window.innerHeight + window.scrollY >= document.body.offsetHeight - 1000 && ready) {
